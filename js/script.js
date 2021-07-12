e4968467790b898b08c524c7706a5fe6639e08dd
habib <habibdev96@gmail.com>
2021-07-11 23:09:16 -0600

    created typewriter effect in phone input

--- js/script.js
+++ js/script.js
@@ -1,1 +1,54 @@
 "use strict";
+const textDisplay = document.querySelector(".phone__footer-input");
+
+const typewriter = () => {
+    const phrases = [
+        "thank you so much!",
+        "how is the walk going?",
+        "i hope she wasn't too much trouble!",
+    ];
+    let i = 0;
+    let j = 0;
+    let currentPhrase = [];
+    let isDeleting = false;
+    let isEnd = false;
+
+    function loop() {
+        isEnd = false;
+        textDisplay.placeholder = currentPhrase.join("");
+
+        if (i < phrases.length) {
+            if (!isDeleting && j <= phrases[i].length) {
+                currentPhrase.push(phrases[i][j]);
+                j++;
+                textDisplay.placeholder = currentPhrase.join("");
+            }
+
+            if (isDeleting && j <= phrases[i].length) {
+                currentPhrase.pop(phrases[i][j]);
+                j--;
+                textDisplay.placeholder = currentPhrase.join("");
+            }
+
+            if (j == phrases[i].length) {
+                isEnd = true;
+                isDeleting = true;
+            }
+
+            if (isDeleting && j === 0) {
+                currentPhrase = [];
+                isDeleting = false;
+                i++;
+                if (i === phrases.length) {
+                    i = 0;
+                }
+            }
+        }
+        const spedUp = Math.random() * (80 - 50) + 50;
+        const normalSpeed = Math.random() * (300 - 200) + 200;
+        const time = isEnd ? 2000 : isDeleting ? spedUp : normalSpeed;
+        setTimeout(loop, time);
+    }
+    loop();
+};
+typewriter();
